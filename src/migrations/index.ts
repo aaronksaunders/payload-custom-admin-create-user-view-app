431e35488e9707d76a65b2f2b7e84ca3e3ac2e54
aaron saunders <aaronksaunders@gmail.com>
2025-01-22 15:39:47 -0500

    feat: add database migration for user and media tables with constraints

---
+++ src/migrations/index.ts
@@ -0,0 +1,9 @@
+import * as migration_20250122_203919 from './20250122_203919';
+
+export const migrations = [
+  {
+    up: migration_20250122_203919.up,
+    down: migration_20250122_203919.down,
+    name: '20250122_203919'
+  },
+];
